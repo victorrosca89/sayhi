0d741d359e3a92b421edb30f452a0fb991bb3d73
Roșca Victor <victorrosca89@gmail.com>
2026-08-21 10:48:58 +0300

    Fix formatting of WebRTC implementation line

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // WebRTC
-    implementation("io.github.webrtc-sdk:android:125.6422.07.1")
+    implementation("io.github.webrtc-sdk:android:125.6422.07.1") 
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
